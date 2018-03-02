6e6caca920b6e5019ecaf59463035ab7ac607a3f
ijsnow <isaacjsnow@gmail.com>
2018-03-02 11:01:19 -0800

    check for babel-polyfill

--- app/util/polyfill.tsx
+++ app/util/polyfill.tsx
@@ -1,6 +1,13 @@
-import 'babel-polyfill'
 import { URL, URLSearchParams } from 'whatwg-url'
 
 const GLOBAL = global as any
 GLOBAL.URL = URL
 GLOBAL.URLSearchParams = URLSearchParams
+
+// We need babel polyfill but don't want to override the one on sourcegraph.com
+// otherwise a needless error will be thrown.
+//
+// Info: https://github.com/babel/babel/issues/4019#issuecomment-354608558
+if (!GLOBAL._babelPolyfill && !(window as any)._babelPolyfill) {
+    require('babel-polyfill')
+}
