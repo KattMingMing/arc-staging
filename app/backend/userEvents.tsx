409e51332923e464a04ccbccd4fa518634007f8f
Farhan Attamimi <farhan@sourcegraph.com>
2018-03-09 11:47:36 -0800

    Fixes

--- app/backend/userEvents.tsx
+++ app/backend/userEvents.tsx
@@ -3,13 +3,6 @@ import { Observable } from 'rxjs/Observable'
 import { map } from 'rxjs/operators/map'
 import { mutateGraphQL } from '../backend/graphql'
 
-type IUserEventEnum = 'PAGEVIEW' | 'SEARCHQUERY'
-
-const getTelligentDuid = (): string | null => {
-    const cookieProps = inspectTelligentCookie()
-    return cookieProps ? cookieProps[0] : null
-}
-
 const inspectTelligentCookie = (): string[] | null => {
     const cookieName = '_te_'
     const matcher = new RegExp(cookieName + 'id\\.[a-f0-9]+=([^;]+);?')
@@ -21,6 +14,12 @@ const inspectTelligentCookie = (): string[] | null => {
     }
 }
 
+const getTelligentDuid = (): string | null => {
+    const cookieProps = inspectTelligentCookie()
+    return cookieProps ? cookieProps[0] : null
+}
+
+type IUserEventEnum = 'PAGEVIEW' | 'SEARCHQUERY'
 const userCookieID = getTelligentDuid()
 export const logUserEvent = (event: IUserEventEnum): Observable<void> =>
     mutateGraphQL(
