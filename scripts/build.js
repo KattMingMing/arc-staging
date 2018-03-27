a1bfa34038a16926cfb2fb11724964542f6c2c42
ijsnow <isaacjsnow@gmail.com>
2018-03-27 15:05:40 -0700

    bundle in prod build

--- scripts/build.js
+++ scripts/build.js
@@ -1,4 +1,6 @@
+var webpack = require('webpack')
 var tasks = require('./tasks')
+var config = require('../webpack/dev.config')
 
 const buildChrome = tasks.buildChrome('prod')
 const buildFirefox = tasks.buildFirefox('prod')
@@ -7,12 +9,23 @@ console.info('[Copy assets]')
 console.info('--------------------------------')
 tasks.copyAssets('prod')
 
+const compiler = webpack(config)
+
 function run() {
-	console.info('[Webpack Build]')
-	console.info('--------------------------------')
-	tasks.buildSafari('prod')
-	buildChrome()
-	buildFirefox()
+    console.info('[Webpack Build]')
+    console.info('--------------------------------')
+
+    compiler.run((err, stats) => {
+        console.log(stats.toString('normal'))
+
+        if (stats.hasErrors()) {
+            return
+        }
+
+        tasks.buildSafari('prod')
+        buildChrome()
+        buildFirefox()
+    })
 }
 
 run()
