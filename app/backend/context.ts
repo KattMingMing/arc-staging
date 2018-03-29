0ea17710b5333866d9d1e45af421909c4c2002ac
ijsnow <isaacjsnow@gmail.com>
2018-03-29 16:14:09 -0700

    fix context for phab bundle

--- app/backend/context.ts
+++ app/backend/context.ts
@@ -5,6 +5,10 @@ export interface RequestContext {
 }
 
 export function getContext(): RequestContext {
+    if (window.SG_ENV === 'PAGE' || window.SOURCEGRAPH_PHABRICATOR_EXTENSION) {
+        return { repoKey: '' }
+    }
+
     const { repoPath } = parseURL()
 
     return { repoKey: repoPath }
